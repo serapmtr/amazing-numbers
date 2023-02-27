001f2ec392f5dfd722dfdd911f737ac0f46ea771
serapbaysal <sbaysal@maritimetrainer.com>
2023-02-27 14:02:19 +0300

    add 6/8

--- main.go
+++ main.go
@@ -14,7 +14,18 @@ type Properties struct {
 	palindromic bool
 	gapful      bool
 	spy         bool
-	count       int
+	square      bool
+	sunny       bool
+	property    []string
+}
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
 
 func Welcome() {
@@ -26,23 +37,24 @@ func Welcome() {
 	fmt.Println("- enter two natural numbers to obtain the properties of the list:")
 	fmt.Println("	* the first parameter represents a starting number;")
 	fmt.Println("	* the second parameter shows how many consecutive numbers are to be printed;")
-	fmt.Println("- two natural numbers and a property to search for;")
+	fmt.Println("- two natural numbers and a two properties to search for;")
 	fmt.Println("- separate the parameters with one space;")
 	fmt.Println("- enter 0 to exit.")
 	fmt.Println()
 
 }
 
-func Request() (string, string, string) {
+func Request() (string, string, string, string) {
 	var number string
 	var size string
 	var property string
+	var secondProperty string
 
 	fmt.Println()
 	fmt.Print("Enter a request: > ")
-	fmt.Scanf("%s %s %s", &number, &size, &property)
+	fmt.Scanf("%s %s %s %s", &number, &size, &property, &secondProperty)
 
-	return number, size, property
+	return number, size, property, secondProperty
 }
 
 func Final(number int, properties Properties) {
@@ -55,11 +67,45 @@ func Final(number int, properties Properties) {
 	fmt.Println("palindromic: ", properties.palindromic)
 	fmt.Println("gapful: ", properties.gapful)
 	fmt.Println("spy: ", properties.spy)
+	fmt.Println("square: ", properties.square)
+	fmt.Println("sunny: ", properties.sunny)
 	fmt.Println()
 
 }
 
-func MultipleFinal(number int, properties Properties) {
+// func FindFinalString(numbers []int, properties Properties) []string {
+// 	final := fmt.Sprintf("%d is", numbers)
+
+// 	if properties.buzz {
+// 		final += " buzz,"
+// 	}
+// 	if properties.duck {
+// 		final += " duck,"
+// 	}
+// 	if properties.even {
+// 		final += " even,"
+// 	}
+// 	if properties.odd {
+// 		final += " odd,"
+// 	}
+// 	if properties.gapful {
+// 		final += " gapful,"
+// 	}
+// 	if properties.palindromic {
+// 		final += " palindromic,"
+// 	}
+// 	if properties.spy {
+// 		final += " spy,"
+// 	}
+// 	if properties.square {
+// 		final += " square,"
+// 	}
+// 	if properties.sunny {
+// 		final += " sunny,"
+// 	}
+// }
+
+func MultipleFinal(number int, properties Properties) []string {
 	fmt.Println()
 	final := fmt.Sprintf("%d is", number)
 	if properties.buzz {
@@ -83,24 +129,33 @@ func MultipleFinal(number int, properties Properties) {
 	if properties.spy {
 		final += " spy,"
 	}
+	if properties.square {
+		final += " square,"
+	}
+	if properties.sunny {
+		final += " sunny,"
+	}
 
 	fmt.Println(final)
+
 	fmt.Println()
 
+	finalArray := strings.Split(final, " ")
+	return finalArray
 }
 
-func Check(number, size int, property string) (*int, *Properties, int) {
+func Check(number, size int, property, secondProperty string) (*int, *Properties, int, []string) {
 	var properties Properties
 	var count = 0
 
 	if !IsNatural(number) {
 		fmt.Println("The first parameter should be a natural number or zero.")
-		return &number, &Properties{}, 0
+		return &number, &Properties{}, 0, properties.property
 	}
 
 	if !IsNatural(size) {
 		fmt.Println("The second parameter should be a natural number.")
-		return &number, &Properties{}, 0
+		return &number, &Properties{}, 0, properties.property
 	}
 
 	even := EvenOrOdd(number)
@@ -115,24 +170,24 @@ func Check(number, size int, property string) (*int, *Properties, int) {
 	isDivisible := IsDivisibleBySeven(subtraction)
 
 	palindromic := IsPalindromic(number)
+
+	properties.property = append(properties.property, secondProperty)
 	if palindromic {
 		properties.palindromic = true
+
 	}
 	if isEnds || isDivisible {
 		properties.buzz = true
-
 	} else {
 		properties.buzz = false
 	}
 
 	if even {
 		properties.even = true
-		properties.odd = false
 
 	} else {
 		properties.even = false
 		properties.odd = true
-
 	}
 
 	if IsDuck(number) {
@@ -147,10 +202,17 @@ func Check(number, size int, property string) (*int, *Properties, int) {
 
 	if IsSpy(number) {
 		properties.spy = true
-
 	}
 
-	return &number, &properties, count
+	if IsSquare(number) {
+		properties.square = true
+	}
+
+	if IsSunny(number) {
+		properties.sunny = true
+
+	}
+	return &number, &properties, count, properties.property
 }
 
 func IsNatural(number int) bool {
@@ -255,58 +317,112 @@ func IsSpy(number int) bool {
 	return false
 }
 
-func Arrays(size, number int, property string) []int {
+func IsSquare(number int) bool {
+	for i := 0; i <= number; i++ {
+		if i*i == number {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsSunny(number int) bool {
+	for i := 0; i < number; i++ {
+		if IsSquare(number + 1) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Arrays(size, number int, property, secondProperty string) []int {
 	var numArray []int
 
-	//propertyArray := [7]string{"even", "odd", "buzz", "duck", "palindromic", "gapful", "spy"}
+	propertyArray := []string{"spy", "even", "odd", "buzz", "palindromic", "gapful", "duck", "square", "sunny"}
 	i := 0
 	if size > 0 {
 		if property != "" {
-			// if  {
-			// 	fmt.Printf("The property [%s] is wrong.\n", property)
-			// 	fmt.Println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]")
-			// 	return numArray
-			// }
 			for i < size {
-				_, properties, _ := Check(number, size, property)
+				_, properties, _, second := Check(number, size, property, secondProperty)
+				if !Contains(propertyArray, property) {
+					fmt.Printf("The property [%s] is wrong.\n", property)
+					fmt.Println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD, SQUARE, SUNNY]")
+					fmt.Println()
+					return numArray
+				}
+
+				if !Contains(propertyArray, secondProperty) {
+					fmt.Printf("The property [%s] is wrong.\n", secondProperty)
+					fmt.Println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD, SQUARE, SUNNY]")
+					fmt.Println()
+					return numArray
+				}
+				if (properties.even && property == "even" && Contains(second, "odd")) || (properties.odd && property == "odd" && Contains(second, "even")) {
+					fmt.Println("The request contains mutually exclusive properties [EVEN, ODD]")
+					fmt.Println("There are no numbers with these properties")
+					return numArray
+				}
+				if (properties.duck && property == "duck" && Contains(second, "spy")) || (properties.spy && property == "spy" && Contains(second, "duck")) {
+					fmt.Println("The request contains mutually exclusive properties [SPY, DUCK]")
+					fmt.Println("There are no numbers with these properties")
+					return numArray
+				}
+				if (properties.sunny && property == "sunny" && Contains(second, "square")) || (properties.square && property == "square" && Contains(second, "sunny")) {
+					fmt.Println("The request contains mutually exclusive properties [SUNNY, SQUARE]")
+					fmt.Println("There are no numbers with these properties")
+					return numArray
+				}
 				if properties.even && property == "even" {
 					numArray = append(numArray, number)
 					number++
 					i++
 
-				} else if properties.odd && property == "odd" {
+				}
+				if properties.odd && property == "odd" {
 					numArray = append(numArray, number)
 					number++
-					i++
-				} else if properties.buzz && property == "buzz" {
-					numArray = append(numArray, number)
-					number++
-					i++
-				} else if properties.duck && property == "duck" {
-					numArray = append(numArray, number)
-					number++
-					i++
-				} else if properties.palindromic && property == "palindromic" {
-					numArray = append(numArray, number)
-					number++
-					i++
-				} else if properties.gapful && property == "gapful" {
-					numArray = append(numArray, number)
-					number++
-					i++
-				} else if properties.spy && property == "spy" {
-					numArray = append(numArray, number)
-					number++
-					i++
-				} else if !strings.Contains(property, "even") || !strings.Contains(property, "odd") || !strings.Contains(property, "buzz") || !strings.Contains(property, "duck") || !strings.Contains(property, "palindromic") || !strings.Contains(property, "gapful") || !strings.Contains(property, "spy") {
-					fmt.Printf("The property [%s] is wrong.\n", property)
-					fmt.Println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]")
-					return numArray
-				} else {
-					number = number + 1
-					size++
 					i++
 				}
+				if properties.buzz && property == "buzz" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.duck && property == "duck" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.palindromic && property == "palindromic" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.gapful && property == "gapful" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.spy && property == "spy" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.square && property == "square" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				if properties.sunny && property == "sunny" {
+					numArray = append(numArray, number)
+					number++
+					i++
+				}
+				number = number + 1
+				size++
+				i++
 
 			}
 		} else {
@@ -317,21 +433,18 @@ func Arrays(size, number int, property string) []int {
 
 	} else {
 
-		oneNum, properties, _ := Check(number, size, property)
+		oneNum, properties, _, _ := Check(number, size, property, secondProperty)
 
 		Final(*oneNum, *properties)
 	}
 	return numArray
 }
 
-func ReadWriteRequests() {
-
-}
-
 func main() {
+	Welcome()
 	for {
-		Welcome()
-		strNumber, strSize, property := Request()
+
+		strNumber, strSize, property, secondProperty := Request()
 		if strNumber == "" {
 			Welcome()
 		}
@@ -342,10 +455,9 @@ func main() {
 			return
 		}
 
-		numArray := Arrays(size, number, property)
+		numArray := Arrays(size, number, property, secondProperty)
 		for i := 0; i < len(numArray); i++ {
-
-			num, properties, _ := Check(numArray[i], size, property)
+			num, properties, _, _ := Check(numArray[i], size, property, secondProperty)
 			if num != nil {
 				MultipleFinal(*num, *properties)
 			}
